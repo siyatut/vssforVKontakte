3b04d8daaf92542f87a26d180a0771893de3bcce
siyatut <anastasiya.tyutinova@gmail.com>
2024-03-22 12:36:07 +0300

    Initial Commit

---
+++ vssforVKontakte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vssforVKontakte
+//
+//  Created by Anastasia Tyutinova on 22/3/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
